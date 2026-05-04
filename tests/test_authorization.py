870ce44e541676517bd7b64641510ff845db67a9
zayka1 <zaycev19991999@gmail.com>
2026-05-04 11:27:41 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

---
+++ tests/test_authorization.py
@@ -0,0 +1,22 @@
+import pytest
+from playwright.sync_api import sync_playwright, expect
+
+
+@pytest.mark.authorization
+@pytest.mark.regression
+@pytest.mark.parametrize("email, password", [("user.name@gmail.com", "password"), ("user.name@gmail.com", " "), (" ", "password")])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill(email)
+
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill(password)
+
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
