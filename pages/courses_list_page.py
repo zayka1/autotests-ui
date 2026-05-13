2502115236c34284fb010e45c6dc63d3f755c483
zayka1 <zaycev19991999@gmail.com>
2026-05-13 15:37:14 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,11 +1,16 @@
 from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
+from components.navigation.sidebar_component import SidebarComponent
+from components.navigation.navbar_component import NavbarComponent
 
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.sidebar = SidebarComponent(page)
+        self.navbar = NavbarComponent(page)
 
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,23 +6,12 @@ from pages.courses_list_page import CoursesListPage
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text('Courses')
-
-    empty_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(empty_icon).to_be_visible()
-
-    empty_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_title).to_be_visible()
-    expect(empty_title).to_have_text('There is no results')
-
-    empty_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(empty_description).to_be_visible()
-    expect(empty_description).to_have_text('Results from the load test pipeline will be displayed here')
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
 
 
 @pytest.mark.courses
