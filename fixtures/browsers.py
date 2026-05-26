e425ce6e9caede50acf7eeafd895c9899a68c036
zayka1 <zaycev19991999@gmail.com>
2026-05-26 16:56:09 +0300

    Запуск автотестов на разных браузерах. Практическое задание: запуск автотестов на нескольких браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -8,9 +8,13 @@ from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param  # Передаем браузер как параметр
+    )
 
 
 @pytest.fixture(scope="session")
@@ -32,10 +36,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state=settings.browser_state_file
+        browser_type=request.param,  # Передаем браузер как параметр
+        storage_state=settings.browser_state_file,
     )
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,19 +1,21 @@
+# tools/playwright/pages.py
 import allure
 from playwright.sync_api import Playwright, Page
 
-from config import settings  # Импортируем настройки
+from config import settings, Browser  # Импортируем enum Browser
 
 
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
+        browser_type: Browser,  # Передаем браузер в качестве аргумента
         storage_state: str | None = None
 ) -> Page:
-
-    browser = playwright.chromium.launch(headless=settings.headless)
+    # Динамически получаем нужный браузер
+    browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
-        base_url=settings.get_base_url(), 
-        storage_state=storage_state, 
+        base_url=settings.get_base_url(),
+        storage_state=storage_state,
         record_video_dir=settings.videos_dir
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
@@ -21,9 +23,8 @@ def initialize_playwright_page(
 
     yield page
 
-    # Используем settings.tracing_dir
     context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
     browser.close()
 
-    # Используем settings.tracing_dir
     allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
+    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
